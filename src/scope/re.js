06346be528e11474be466de13f0552ebd7b64bcb
Javier <javierorozco0112@gmail.com>
2022-10-14 11:11:54 -0500

    Diferencias entre Let, Const y Var

---
+++ src/scope/re.js
@@ -0,0 +1,22 @@
+var firstName; //undefined 
+
+firstName = 'Javier';
+console.log(firstName);
+
+var lastName = 'David'; // Declarar / asignar
+lastName = 'Ana'; // Reasignando 
+console.log(lastName);
+
+
+// Redeclaracion y Reasignacion 
+
+var secondName = 'David'; // declarando / asignando
+var secondName = 'Ana'; // reasignando 
+console.log(secondName);
+
+// Let 
+
+let fruit = 'Apple'; // Declarar / asignar
+
+fruit = 'Kiwi'; // reasignar 
+
